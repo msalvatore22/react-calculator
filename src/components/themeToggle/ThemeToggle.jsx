be2fe5f296a77c3dafe62056522ab790c047af27
Michael Salvatore <msalvatore22@gmail.com>
2023-07-10 16:50:38 -0400

    Three way toggle buttons working

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,20 +1,39 @@
 import "./themeToggle.scss"
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import ThemeContext from '../../contexts/ThemeContext'
 
-const themeOptions = [{ value: "theme1" }, { value: "theme2" }, { "value": "theme3"}];
 
 const ThemeToggle = () => {
+  let themes = [{ value: "theme1", active: true }, { value: "theme2", active: false }, { value: "theme3", active: false}];
   const { theme, setTheme } = useContext(ThemeContext)
+  const [themeOptions, setThemeOptions] = useState(themes)
+
+  const handleClick = (idx, option) => {
+    setTheme(option.value)
+
+    let copy = themeOptions
+    copy.forEach(op => {
+      op.active = false
+    })
+
+    copy[idx]["active"] = true
+    setThemeOptions(copy)
+  }
+
+
 
   return (
     <div className="toggle-box">
       <h1>calc</h1>
-      <div>
+      <p>theme</p>
+      <div className='theme-toggle'>
         {
-          themeOptions.map((option,idx) => (
-            <button key={idx} onClick={() => setTheme(option.value)}>
-              {idx+1}
+          themeOptions.map((option, idx) => (
+            <button 
+              className={option.active ? 'active' : null }
+              key={idx}
+              onClick={(e) => handleClick(idx, option)}
+            >
             </button>
           ))
         }
